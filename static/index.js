554ddb843ab5e888d7d57531059d92d1439194cf
Alon Jacobson <alon.jacobson@tufts.edu>
2019-11-28 22:48:28 -0500

    a little too much playing around with ui

--- static/index.js
+++ static/index.js
@@ -43,7 +43,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
             // (c)
-            // // const cardHeader = $('<div class="card-header" data-toggle="collapse">')
+            // const cardHeader = $('<div class="card-header" data-toggle="collapse">')
             // const cardHeader = $('<div class="card-header btn-toolbar justify-content-between" data-toggle="collapse">')
             //     .attr({
             //         "data-target": `#collapser${course.id}`,
@@ -52,15 +52,94 @@ document.addEventListener('DOMContentLoaded', () => {
             //     }).append($(`<span>${course.course_num} - ${course.title}</span>`))
             //     .append($('<button id="button" class="btn btn-primary" type="button">Add</button>'));
 
+
             // (d)
-            const cardHeader = $('<div class="card-header btn-toolbar justify-content-between col-md-11" data-toggle="collapse">')
+            // const cardHeader = $('<div class="card-header row">')
+            //     .append(
+            //         $(`<span class="col-sm" data-toggle="collapse">${course.course_num} - ${course.title}</span>`)
+            //             .attr({
+            //                 "data-target": `#collapser${course.id}`,
+            //                 "aria-controls": `collapser${course.id}`,
+            //                 "id": `header${course.id}`
+            //             })
+            //     )
+            //     .append(
+            //         // $('<div class="col-sm"></div>')
+            //         //     .append(
+            //         //         $('<button id="button" class="btn btn-primary" type="button">Add</button>')
+            //         //     )
+            //         $('<div class="col-sm"></div>')
+            //             .append(
+            //                 $('<div class="row justify-content-end btn-toolbar"></div>')
+            //                     .append(
+            //                         $('<button id="button" class="btn btn-primary" type="button">Add</button>')
+            //                     )
+            //             )
+            //     );
+
+            // (e)
+            // const cardHeader = $('<div class="row">')
+            //     .append(
+            //         $(`<span class="col-md card-header" data-toggle="collapse">${course.course_num} - ${course.title}</span>`)
+            //             .attr({
+            //                 "data-target": `#collapser${course.id}`,
+            //                 "aria-controls": `collapser${course.id}`,
+            //                 "id": `header${course.id}`
+            //             })
+            //     )
+            //     .append(
+            //         $('<div class="col-md"></div>')
+            //             .append(
+            //                 $('<div class="row justify-content-end btn-toolbar card-header"></div>')
+            //                     .append(
+            //                         $('<button id="button" class="btn btn-primary" type="button">Add</button>')
+            //                     )
+            //             )
+            //     );
+
+            // (f)
+            // const buttonInCardHeader = $('<div data-toggle="collapse">')
+            //     .attr({
+            //         "data-target": `#collapser${course.id}`,
+            //         "aria-controls": `collapser${course.id}`
+            //     }).text(course.course_num + ' - ' + course.title);
+
+            // const cardHeader = $('<div class="card-header">')
+            //     .attr("id", `header${course.id}`)
+            //     .append(buttonInCardHeader);
+
+
+
+            // // (g)
+            // const buttonInCardHeader = $('<div data-toggle="collapse" class="float-left">')
+            //     .attr({
+            //         "data-target": `#collapser${course.id}`,
+            //         "aria-controls": `collapser${course.id}`
+            //     }).text(course.course_num + ' - ' + course.title);
+            // const cardHeader = $('<div class="card-header">')
+            //     .attr("id", `header${course.id}`)
+            //     .append(buttonInCardHeader)
+            //     .append($('<button id="button" class="btn btn-primary float-right" type="button">Add</button>'));
+
+
+            // (h)
+            const buttonInCardHeader = $('<div class="float-left">')
+                .text(course.course_num + ' - ' + course.title);
+            const cardHeaderLeft = $('<div data-toggle="collapse" class="col">')
                 .attr({
                     "data-target": `#collapser${course.id}`,
                     "aria-controls": `collapser${course.id}`,
                     "id": `header${course.id}`
-                }).append($(`<span>${course.course_num} - ${course.title}</span>`))
-                .append($('<button id="button" class="btn btn-primary" type="button">Add</button>'));
+                })
+                .append(buttonInCardHeader);
+            const cardHeaderRight = $('<button id="button" class="btn btn-primary float-right" type="button">Add</button>');
+            const cardHeader = $('<div class="card-header row"></div>').append(cardHeaderLeft).append(cardHeaderRight);
 
+
+
+                
+
+            
 
             const cardBody = $('<div class="card-body"></div>')
                 .append(
