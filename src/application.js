c5b387cd95aa9e02f96830e5ba243a93afe1f2cf
Alon Jacobson <alon.jacobson@tufts.edu>
2019-12-29 14:23:43 +0000

    load scripts and stylesheets from CDNs so it is faster

--- src/application.js
+++ src/application.js
@@ -3,11 +3,6 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-// // https://www.tiktip.com/a/1/4/15/1
-// const mustacheExpress = require('mustache-express');
-// app.engine('html', mustacheExpress());
-// app.set('view engine', 'html');
-
 app.set('view engine', 'ejs');
 
 app.use(express.static('src/public'));
@@ -103,20 +98,17 @@ get_data.load_all_course_data(vals => {
 }, console.error, false);
 
 function startServer() {
-    // updated all data every so often
+    // update all data every so often
     setInterval(function() {
         console.log("Updating all data...");
         get_data.load_course_data(undefined, ()=>{console.log("Successfully updated all data!")}, console.error, true);
-    }, 60 * 5 * 1000);
+    }, 60 * 10 * 1000);
 
     app.get('/', function(req, res) {
         res.render('index', {terms: Object.keys(models.term_to_code)});
     });
 
     app.get('/schedule', function(req, res) {
-        console.log(req.query);
-
-
         if (!/^((\d+-)+\d+|\d*)$/.test(req.query.ids)) {
             res.send("Wrong format!");
             return;
@@ -136,7 +128,6 @@ function startServer() {
             consecutiveness_weight: parseInt(req.query.pref_consecutive, 10) / 100,
         };
 
-        // TODO: get this from the user
         // const score_function = _ => Math.random();
         const score_function = schedule => schedule_stats.get_score(schedule, req.query.term, weights)
         const k = 100;
@@ -146,9 +137,9 @@ function startServer() {
                     return false
             return true
         }
-        // tic('generate schedules');
+        tic('generate schedules');
         const info = get_top_schedules_list(ids, accepted_statuses, score_function, k, section_accept_function, req.query.term);
-        // toc('generate schedules');
+        toc('generate schedules');
         console.log(`n schedules: ${info.n_possibilities}`);
         res.render('schedule', {data: JSON.stringify(info)});
     });
@@ -168,44 +159,101 @@ function startServer() {
     http.listen(PORT, function() {
         console.log(`Listening on ${PORT}`);
     });
-
-    function get_top_schedules_list(course_ids, accepted_statuses, score_function, k, section_accept_function, term) {   tic('generate schedules');
-        // array of Course objects
-        const courses = course_ids.map(id => models.courses[term][id]);
-        // generator of "schedules" which are represented as a 2D arrays.
-        // Each element of a schedule is an array containing the IDs of Sections.
-        const schedules = get_schedules(courses, accepted_statuses, section_accept_function, term);                      toc('generate schedules');  tic('sort schedules');
-        // generator of objects with schedules and their scores
-        const schedules_and_scores = (function* () {
-            for (const schedule of schedules) {
-                yield {
-                    schedule: schedule,
-                    score: score_function(schedule)
-                };
-            }
-        })();
-        // object that gets the k schedules with the highest scores and sorts them
-        const sorter = new PartialSorter((a, b) => {
-            var cmp = default_compare(b.score, a.score);
-            if (cmp === 0)
-                return Math.random() - 0.5;
-            return cmp;
-        }, k);
-        sorter.insertAll(schedules_and_scores);                                                                          toc('sort schedules');
-        return {
-            n_possibilities: sorter.numPassed,
-            top_schedules: sorter.getMinArray(),
-            courses: courses
-        };
-    }
-
-    function get_schedules(courses, accepted_statuses, section_accept_function, term) {
-        const pg = new course_scheduler.PeriodGroup(
-            courses.map(course => 
-                api.course_object_to_period_group(course, true, accepted_statuses, cache=false, give_ids=true, section_accept_function, term)
-            ),
-            'and', merge=false, cache=false, null, term
-        );
-        return pg.evaluate();
-    }
 }
+
+function get_top_schedules_list(course_ids, accepted_statuses, score_function, k, section_accept_function, term) {
+    // array of Course objects
+    const courses = course_ids.map(id => models.courses[term][id]);
+    // generator of "schedules" which are represented as a 2D arrays.
+    // Each element of a schedule is an array containing the IDs of Sections.
+    const schedules = get_schedules(courses, accepted_statuses, section_accept_function, term);
+    // generator of objects with schedules and their scores
+    const schedules_and_scores = (function* () {
+        for (const schedule of schedules) {
+            yield {
+                schedule: schedule,
+                score: score_function(schedule)
+            };
+        }
+    })();
+    // object that gets the k schedules with the highest scores and sorts them
+    const sorter = new PartialSorter((a, b) => {
+        var cmp = default_compare(b.score, a.score);
+        if (cmp === 0)
+            return Math.random() - 0.5;
+        return cmp;
+    }, k);
+    sorter.insertAll(schedules_and_scores);
+    return {
+        n_possibilities: sorter.numPassed,
+        top_schedules: sorter.getMinArray(),
+        courses: courses
+    };
+}
+
+function get_schedules(courses, accepted_statuses, section_accept_function, term) {
+    const pg = new course_scheduler.PeriodGroup(
+        courses.map(course =>
+            api.course_object_to_period_group(course, true, accepted_statuses, cache=false, give_ids=true, section_accept_function, term)
+        ),
+        'and', merge=false, cache=false, null, term
+    );
+    return pg.evaluate();
+}
+
+function get_top_schedules_list_timing_test(course_ids, accepted_statuses, score_function, k, section_accept_function, term) {
+    /* TIMING TEST - NOT FOR PRODUCTION */
+    const courses = course_ids.map(id => models.courses[term][id]);
+
+    const schedules1 = get_schedules(courses, accepted_statuses, section_accept_function, term);
+    const schedules2 = get_schedules(courses, accepted_statuses, section_accept_function, term);
+    const schedules3 = get_schedules(courses, accepted_statuses, section_accept_function, term);
+
+    const schedules_and_scores2 = (function* () {
+        for (const schedule of schedules2) {
+            yield {
+                schedule: schedule,
+                score: score_function(schedule)
+            };
+        }
+    })();
+    const schedules_and_scores3 = (function* () {
+        for (const schedule of schedules3) {
+            yield {
+                schedule: schedule,
+                score: score_function(schedule)
+            };
+        }
+    })();
+
+    const sorter3 = new PartialSorter((a, b) => {
+        var cmp = default_compare(b.score, a.score);
+        if (cmp === 0)
+            return Math.random() - 0.5;
+        return cmp;
+    }, k);
+
+    tic('schedule generate');
+    for (const s of schedules1) {
+
+    }
+    toc('schedule generate');
+
+    tic('schedule generate and add score');
+    for (const s of schedules_and_scores2) {
+
+    }
+    toc('schedule generate and add score');
+
+    tic('schedule generate and add score and sort');
+    sorter3.insertAll(schedules_and_scores3);
+    toc('schedule generate and add score and sort');
+
+    return {
+        n_possibilities: sorter3.numPassed,
+        top_schedules: sorter3.getMinArray(),
+        courses: courses
+    };
+}
+
+
