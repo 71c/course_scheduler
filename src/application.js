4a90e7a555e42a2aefee1eab578cb4d28e80b42c
Alon Jacobson <alon.jacobson@tufts.edu>
2020-01-07 20:49:05 +0000

    added ability to easily switch whether js & css are retrieved from a CDN

--- src/application.js
+++ src/application.js
@@ -4,31 +4,118 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 const UPDATE_INTERVAL = 30; // every UPDATE_INTERVAL minutes it updates all the course data
+const OFFLINE_MODE = true; // I use offline mode when I don't have WiFi
+const USE_CDN = process.env.NODE_ENV === 'production' || !OFFLINE_MODE;
+
+const RESOURCES = {
+    css: {
+        'bootstrap': {
+            cdn: '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">',
+            local: '<link rel="stylesheet" href="bootstrap/dist/css/bootstrap.min.css">'
+        },
+        'jquery-ui': {
+            cdn: '<link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.min.css" integrity="sha384-kcAOn9fN4XSd+TGsNu2OQKSuV5ngOwt7tg73O4EpaD91QXvrfgvf0MR7/2dUjoI6" crossorigin="anonymous">',
+            local: '<link rel="stylesheet" href="jquery-ui.min.css">'
+        },
+        '@fullcalendar/core': {
+            cdn: '<link rel="stylesheet" href="https://unpkg.com/@fullcalendar/core@4.3.1/main.min.css" integrity="sha384-UYbBlSMkHrbuUVqSs26Rm1UEii5VOTR80mD2wjrJaIedgHgS5LWDt9d7rcbqEDxR" crossorigin="anonymous">',
+            local: '<link rel="stylesheet" href="@fullcalendar/core/main.min.css">'
+        },
+        '@fullcalendar/timegrid': {
+            cdn: '<link rel="stylesheet" href="https://unpkg.com/@fullcalendar/timegrid@4.3.0/main.min.css" integrity="sha384-Y2E7KGXSxkviA14jxA5DeBmVwovKH3ipCOXJ1ByetT9DLlwehyYf/3Lm2wSncMR2" crossorigin="anonymous">',
+            local: '<link rel="stylesheet" href="@fullcalendar/timegrid/main.min.css">'
+        },
+        'main': {
+            local: '<link rel="stylesheet" href="./main.css">'
+        }
+    },
+    js: {
+        'jquery': {
+            cdn: '<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha384-vk5WoKIaW/vJyUAd9n/wmopsmNhiy+L2Z+SBxGYnUkunIxVxAv/UtMOhba/xskxh" crossorigin="anonymous" defer></script>',
+            local: '<script src="jquery/dist/jquery.min.js" defer></script>'
+        },
+        'popper.js': {
+            cdn: '<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous" defer></script>',
+            local: '<script src="popper.js/dist/umd/popper.min.js" defer></script>'
+        },
+        'bootstrap': {
+            cdn: '<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous" defer></script>',
+            local: '<script src="bootstrap/dist/js/bootstrap.min.js" defer></script>'
+        },
+        'jquery-ui': {
+            cdn: '<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous" defer></script>',
+            local: '<script src="jquery-ui.min.js" defer></script>'
+        },
+        'jqueryui-touch-punch': {
+            cdn: '<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui-touch-punch/0.2.3/jquery.ui.touch-punch.min.js" integrity="sha384-MI/QivrbkVVJ89UOOdqJK/w6TLx0MllO/LsQi9KvvJFuRHGbYtsBvbGSM8JHKCS0" crossorigin="anonymous" defer></script>'
+        },
+        '@fullcalendar/core': {
+            cdn: '<script src="https://unpkg.com/@fullcalendar/core@4.3.1/main.min.js" integrity="sha384-m/iMkOZWaAFStlxoUSSGM0Ow9kDwNOIzw2aQDNC3SFffa50jU2HSuluMYODsvAGR" crossorigin="anonymous" defer></script>',
+            local: '<script src="@fullcalendar/core/main.min.js" defer></script>'
+        },
+        '@fullcalendar/daygrid': {
+            cdn: '<script src="https://unpkg.com/@fullcalendar/daygrid@4.3.0/main.min.js" integrity="sha384-Vg8WM1KZn59u30ahJ4mGZwEw2R4ieRhhvX2PPiKKCSpuxms+HvoC3XPii6CnMvFD" crossorigin="anonymous" defer></script>',
+            local: '<script src="@fullcalendar/daygrid/main.min.js" defer></script>'
+        },
+        '@fullcalendar/timegrid': {
+            cdn: '<script src="https://unpkg.com/@fullcalendar/timegrid@4.3.0/main.min.js" integrity="sha384-yyTZuDPNQFp/xKe4Yk67GZBC6Pm3OXdipzs78KRRA37YWJuy3rcgDQTxCxbIviHs" crossorigin="anonymous" defer></script>',
+            local: '<script src="@fullcalendar/timegrid/main.min.js" defer></script>'
+        },
+        'index': {
+            local: '<script src="index.js" defer></script>'
+        },
+        'schedule': {
+            local: '<script src="./schedule.js" defer></script>'
+        }
+    }
+}
+
+const RESOURCES_USED = {
+    index: {
+        css: ['bootstrap', 'jquery-ui', 'main'],
+        js: ['jquery', 'popper.js', 'bootstrap', 'jquery-ui', 'index', 'jqueryui-touch-punch']
+    },
+    schedule: {
+        css: ['bootstrap', 'main', '@fullcalendar/core', '@fullcalendar/timegrid'],
+        js: ['jquery', '@fullcalendar/core', '@fullcalendar/daygrid', '@fullcalendar/timegrid', 'popper.js', 'bootstrap', 'schedule']
+    }
+}
+
+function getResources() {
+    const resourcesStrings = {};
+    for (const view in RESOURCES_USED) {
+        resourcesStrings[view] = {};
+        for (const kind in RESOURCES_USED[view]) {
+            const theseResources = [];
+            for (const name of RESOURCES_USED[view][kind]) {
+                console.log(kind, name);
+                const options = RESOURCES[kind][name];
+                if (options === undefined) {
+                    const err = new Error(`${kind} resource named ${name} is not included in RESOURCES`);
+                    throw err;
+                }
+                if (USE_CDN) {
+                    if ('cdn' in options)
+                        theseResources.push(options.cdn);
+                    else
+                        theseResources.push(options.local);
+                }
+                else {
+                    if ('local' in options)
+                        theseResources.push(options.local);
+                }
+            }
+            resourcesStrings[view][kind] = theseResources;
+        }
+    }
+    return resourcesStrings;
+}
+
+const RESOURCES_STRINGS = getResources();
 
 app.set('view engine', 'ejs');
 app.set('views', 'src/public');
 
-function sslwwwRedirect(useWWW) {
-    var checkWWW = useWWW ?
-        req => req.subdomains.length === 0 : // no www
-        req => req.subdomains.length === 1 && req.subdomains[0] === 'www'; // there is www
-    var changeWWW = useWWW ? 
-        req => 'https://www.' + req.headers.host + req.originalUrl : // add www back
-        req => 'https://' + req.headers.host.slice(4) + req.originalUrl; // remove www
-    return function(req, res, next) {
-        if (req.hostname === 'localhost' || process.env.NODE_ENV === 'development')
-            // don't redirect if http://localhost or development
-            next();
-        else if (checkWWW(req))
-            res.redirect(301, changeWWW(req));
-        else if ((req.headers['x-forwarded-proto'] || req.protocol) === 'https')
-            // uses https and there is no www; don't redirect
-            next();
-        else
-            // using http; redirect to use https
-            res.redirect(301, 'https://' + req.headers.host + req.originalUrl);
-    };
-}
 app.use(sslwwwRedirect(false));
 
 const compression = require('compression');
@@ -64,30 +151,6 @@ function toc(name) {
     return dt;
 }
 
-function minutesToTimeString12hr(minutes) {
-    var minutePart = minutes % 60;
-    var hourPart = Math.floor(minutes / 60) % 24;
-    var amPm = hourPart >= 12 ? "PM" : "AM";
-    hourPart = hourPart % 12;
-    if (hourPart === 0)
-        hourPart = 12;
-    var hourPartString = hourPart.toString();
-    var minutePartString = minutePart > 9 ? minutePart.toString() : "0" + minutePart.toString();
-    return hourPartString + ":" + minutePartString + amPm;
-}
-function groupBy(items, groupFunction) {
-    const map = new Map();
-    for (const item of items) {
-        const value = groupFunction(item);
-        if (map.has(value)) {
-            map.get(value).push(item);
-        } else {
-            map.set(value, [item]);
-        }
-    }
-    return [...map.values()];
-}
-
 tic('load the data')
 get_data.load_all_course_data(vals => {
     toc('load the data');
@@ -103,7 +166,11 @@ function startServer() {
     }, UPDATE_INTERVAL * 60*1000);
 
     app.get('/', function(req, res) {
-        res.render('index', {terms: Object.keys(models.term_to_code)});
+        res.render('index', {
+            terms: Object.keys(models.term_to_code),
+            css: RESOURCES_STRINGS.index.css,
+            js: RESOURCES_STRINGS.index.js
+        });
     });
 
     app.get('/schedule', function(req, res) {
@@ -139,7 +206,13 @@ function startServer() {
         const info = get_top_schedules_list(ids, accepted_statuses, score_function, k, section_accept_function, req.query.term);
         toc('generate schedules');
         console.log(`n schedules: ${info.n_possibilities}`);
-        res.render('schedule', {data: JSON.stringify(info), n_possibilities: info.n_possibilities, k: k});
+        res.render('schedule', {
+            data: JSON.stringify(info),
+            n_possibilities: info.n_possibilities,
+            k: k,
+            css: RESOURCES_STRINGS.schedule.css,
+            js: RESOURCES_STRINGS.schedule.js
+        });
     });
 
     app.get('/search', function(req, res) {
@@ -295,4 +368,39 @@ function max(arr, {key=x=>x, compareFunction=default_compare}={}) {
         }
     }
     return best;
+}
+
+function groupBy(items, groupFunction) {
+    const map = new Map();
+    for (const item of items) {
+        const value = groupFunction(item);
+        if (map.has(value)) {
+            map.get(value).push(item);
+        } else {
+            map.set(value, [item]);
+        }
+    }
+    return [...map.values()];
+}
+
+function sslwwwRedirect(useWWW) {
+    var checkWWW = useWWW ?
+        req => req.subdomains.length === 0 : // no www
+        req => req.subdomains.length === 1 && req.subdomains[0] === 'www'; // there is www
+    var changeWWW = useWWW ? 
+        req => 'https://www.' + req.headers.host + req.originalUrl : // add www back
+        req => 'https://' + req.headers.host.slice(4) + req.originalUrl; // remove www
+    return function(req, res, next) {
+        if (req.hostname === 'localhost' || process.env.NODE_ENV === 'development')
+            // don't redirect if http://localhost or development
+            next();
+        else if (checkWWW(req))
+            res.redirect(301, changeWWW(req));
+        else if ((req.headers['x-forwarded-proto'] || req.protocol) === 'https')
+            // uses https and there is no www; don't redirect
+            next();
+        else
+            // using http; redirect to use https
+            res.redirect(301, 'https://' + req.headers.host + req.originalUrl);
+    };
 }
